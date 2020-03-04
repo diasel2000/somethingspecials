2ec127de7145d35f600280e7f68f0be293cf38fa
Anatolii <n221296@gmail.com>
2020-03-04 13:15:57 +0200

    Initial commit

---
+++ src/main/java/com/somespecial/specials/SpecialsApplication.java
@@ -0,0 +1,13 @@
+package com.somespecial.specials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialsApplication.class, args);
+	}
+
+}
